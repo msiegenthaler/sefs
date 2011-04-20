ef7ba49c101d1ad14c356ddd4bcc0e1bc5515f13
msiegenthaler <msiegenthaler@gmx.net>
2011-04-20 22:43:43 +0200

    added CPS-based implementation for IO's

---
+++ src/sefs/cps/package.scala
@@ -0,0 +1,24 @@
+package sefs
+
+import effect.{ IO, IOMonad }
+import scala.util.continuations._
+
+package object cps {
+
+  class IOCps[A] private[cps] (v: IO[A]) {
+    def value: A @cps[IO[Any]] = shift(c => v flatMap c)
+    def exec = value
+  }
+  
+  implicit def io2cps[A](a: IO[A]) = new IOCps(a)
+
+  /** Converts a CPS-IO to a regular IO */
+  def asIO[A](cps: => A @cps[IO[Any]]): IO[A] = {
+    val ctx = reify[A, IO[Any], IO[Any]] {
+      cps
+    }
+    val r = ctx.foreach(x => IOMonad.pure(x))
+    r.asInstanceOf[IO[A]]
+  }
+
+}
---
+++ src/sefs/test/CpsIOTest.scala
@@ -0,0 +1,25 @@
+package sefs
+package test
+
+import effect.IOPerformer
+import cps._
+import io._
+
+object CpsIOTest extends App with IOPerformer {
+  val o = asIO {
+    ConsoleIO.println("One").exec
+    ConsoleIO.println("Two").exec
+    if (1 > 2)
+      ConsoleIO.println("False").exec
+    else
+      ConsoleIO.println("True").exec
+    "Three"
+  }
+
+  println("****")
+  val result = perform(o)
+  println("Result=" + result)
+  println("Upper Result=" + result.toUpperCase)
+  println("****")
+
+}
